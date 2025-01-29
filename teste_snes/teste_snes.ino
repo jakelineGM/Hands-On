82ed940a224ec304f7d5e1891e596c0a86a4539f
jakelineGM <jakufam2020@gmail.com>
2025-01-28 20:15:58 -0400

    Atualização dia 28_01

--- teste_snes/teste_snes.ino
+++ teste_snes/teste_snes.ino
@@ -1,36 +1,43 @@
+int pinDados = 22; // declara o pino de dados na porta 22
+int pinClock = 1; // declara o pino de clock na porta 1
+int pinLatch = 23; // declara o pino de dados na porta 23
 
-int pinDados = 22; // declara o pino de dados na porta 12
-int pinClock = 1; // declara o pino de clock na porta 14
-int pinLatch = 23; // declara o pino de dados na porta 13
+void sendLatchSignal() {
+    digitalWrite(pinLatch, HIGH);
+    delayMicroseconds(12);
+    digitalWrite(pinLatch, LOW);
+    Serial.println("Latch enviado");
+}
+
+void sendClockPulses() {
+    for (int i = 0; i < 9; i++) {
+        digitalWrite(pinClock, HIGH);
+        delayMicroseconds(6); // Meio ciclo
+        digitalWrite(pinClock, LOW);
+        delayMicroseconds(6); // Meio ciclo
+
+        delay(800);
+
+        // Ler valor do pino de dados
+        int bitValue = digitalRead(pinDados);
+        Serial.printf("Bit %d lido: %d\n", i, bitValue);
+
+    }
+}
 
 
 void setup() {
-  pinMode(pinLatch, OUTPUT); // define o pino Latch como entrada
-  pinMode(pinClock, OUTPUT); // define o pino Clock como entrada
-  pinMode(pinDados, INPUT); // define o pino data como saída
+  pinMode(pinLatch, OUTPUT); // define o pino Latch como saída
+  pinMode(pinClock, OUTPUT); // define o pino Clock como saída
+  pinMode(pinDados, INPUT); // define o pino data como entrada
   
   Serial.begin(115200);
+  Serial.println("Simulador SNES iniciado...");
 
-  delay(5000);
-  Serial.println("Comecou");
-
-
-  digitalWrite(pinLatch, LOW);
-  digitalWrite(pinClock, LOW);
-  digitalWrite(pinLatch, HIGH);
-  digitalWrite(pinLatch, LOW);
-  digitalWrite(pinClock, HIGH);
-  digitalWrite(pinClock, LOW);
-  Serial.println(digitalRead(pinLatch));
-  Serial.println(digitalRead(pinClock));
-  Serial.println(digitalRead(pinDados));
-  digitalWrite(pinLatch, LOW);
-  digitalWrite(pinClock, LOW);
-  
-
+   sendLatchSignal();
+   sendClockPulses();
 }
 
 void loop() {
-  
-
+    
 }
